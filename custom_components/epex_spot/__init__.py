cb64ab8793fcd02b8433ac21017fd6c345afb9d2
mampfes <mampfes@users.noreply.github.com>
2023-02-02 19:43:57 +0100

    fix warning in HA 2023.2

--- custom_components/epex_spot/__init__.py
+++ custom_components/epex_spot/__init__.py
@@ -24,7 +24,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # add market area to shell
     shell.add_entry(entry)
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
