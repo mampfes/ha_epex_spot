b34e35cf6fd0d82e30162ba3d2ed81490e3589c0
suaveolent <suaveolent@users.noreply.github.com>
2024-11-22 09:27:03 +0100

    fix epex spot web scraper url

--- custom_components/epex_spot/EPEXSpot/EPEXSpotWeb/__init__.py
+++ custom_components/epex_spot/EPEXSpot/EPEXSpotWeb/__init__.py
@@ -70,7 +70,7 @@ class Marketprice:
 
 
 class EPEXSpotWeb:
-    URL = "https://www.epexspot.com/en/market-data"
+    URL = "https://www.epexspot.com/en/market-results"
 
     MARKET_AREAS = (
         "AT",
@@ -197,7 +197,7 @@ class EPEXSpotWeb:
         }
 
         async with self._session.post(
-            self.URL, headers=headers, params=params, data=data, verify_ssl=False
+            self.URL, headers=headers, params=params, data=data, verify_ssl=True
         ) as resp:
             resp.raise_for_status()
             return await resp.json()
